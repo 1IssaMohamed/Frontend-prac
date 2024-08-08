383d68866587f5bbd0a6c2ff00dcedeb1fe52477
1IssaMohamed <issamoh@my.yorku.ca>
2024-08-08 17:35:28 -0400

    continuing to work thorugh the blackjack questions, some of my own side work, 25% doen section!

--- JS/blackJack.js
+++ JS/blackJack.js
@@ -1,21 +1,54 @@
-let first=2 
-let second=6
-sum=first+second
+let first=0
+let second =0
+let sum =(first+second)
 let hasBlackJack=false
 let isAlive=true
+let sumText = document.querySelector("#sum")
+let cards=document.querySelector("#cards")
+let message=document.getElementById("message-el")
+
 // <21 sitll in the game
 // ==21 YOOOHOOOO
 // >21 fml
-let mesage=""
-if(sum<21){
-    message= ("Want another card??")
-}
-else if(sum==21){
-    message=("YOU DID IT BOY")
-    hasBlackJack=true
-}
-else{
-    message=("You never know if theres diamonds waitinging right behind this layer of rocks\n keep going!!")
-    isAlive=false
-}
+function start(){
+    first= Math.floor(Math.random()*13)
+    second= Math.floor(Math.random()*13)
+    cards.textContent=("Cards:"+first+","+second)
+    sumText.textContent=("Sum:"+(first+second))
+    let sum= (first+second)
+    if(sum<21){
+        message.textContent= ("Want another card??")
+    }
+    else if(sum==21){
+        message.textContent=("YOU DID IT BOY")
+        hasBlackJack=true
+    }
+    else{
+        message.textContent=("You never know if theres diamonds waitinging right behind this layer of rocks\n keep going!!")
+        isAlive=false
+    }
 console.log(message)
+
+}
+function card(){
+    newC=Math.floor(Math.random()*13)
+    let sumText = document.querySelector("#sum")
+    sum=parseInt(sumText.value)+newC
+
+    let cards=document.querySelector("#cards")
+    let message=document.getElementById("message-el")
+    sumText.textContent=("Sum:"+sum)
+    cards.textContent+=(","+newC)
+    if(sum<21){
+        message.textContent= ("Want another card??")
+    }
+    else if(sum==21){
+        message.textContent=("YOU DID IT BOY")
+        hasBlackJack=true
+    }
+    else{
+        message.textContent=("You never know if theres diamonds waitinging right behind this layer of rocks\n keep going!!")
+        isAlive=false
+    }
+console.log(message)
+}
