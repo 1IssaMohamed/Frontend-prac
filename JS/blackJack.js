4f46de96e10c3ff521b54e435e8538d9d44c4c23
1IssaMohamed <issamoh@my.yorku.ca>
2024-08-09 17:50:22 -0400

    finsihed bj game,50% done course

--- JS/blackJack.js
+++ JS/blackJack.js
@@ -1,21 +1,45 @@
-let first=0
-let second =0
-let sum =(first+second)
+let sum= 0
+let allCards=[]
 let hasBlackJack=false
 let isAlive=true
 let sumText = document.querySelector("#sum")
 let cards=document.querySelector("#cards")
 let message=document.getElementById("message-el")
+// creating a player object that tracks how much money I really got 
+let player = {
+    name: "John",
+    chips: 145,
+    sayHello: function(){
+        console.log("YOOOOOOOOOO!")
+    }
+}
 
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
 // <21 sitll in the game
 // ==21 YOOOHOOOO
 // >21 fml
 function start(){
-    first= Math.floor(Math.random()*13)
-    second= Math.floor(Math.random()*13)
-    cards.textContent=("Cards:"+first+","+second)
-    sumText.textContent=("Sum:"+(first+second))
-    let sum= (first+second)
+    // let isAlive=true
+    first= Math.floor(Math.random()*13)+1
+    second= Math.floor(Math.random()*13)+1
+    sum= (first+second)
+    isAlive=true
+    hasBlackJack=false
+    allCards=[first,second]
+    render()
+}
+function render(){
+    if(isAlive==false){
+        message.textContent=("You lost already bro give it up :skull:")
+    }
+    else if(hasBlackJack==true){
+        message.textContent=("You won already, wanna give me my money back?")
+    }
+    else{
+    sumText.textContent=("Sum: "+sum)
+    cards.textContent=("Cards: "+allCards)
     if(sum<21){
         message.textContent= ("Want another card??")
     }
@@ -27,28 +51,11 @@ function start(){
         message.textContent=("You never know if theres diamonds waitinging right behind this layer of rocks\n keep going!!")
         isAlive=false
     }
-console.log(message)
-
+}
 }
 function card(){
-    newC=Math.floor(Math.random()*13)
-    let sumText = document.querySelector("#sum")
-    sum=parseInt(sumText.value)+newC
-
-    let cards=document.querySelector("#cards")
-    let message=document.getElementById("message-el")
-    sumText.textContent=("Sum:"+sum)
-    cards.textContent+=(","+newC)
-    if(sum<21){
-        message.textContent= ("Want another card??")
-    }
-    else if(sum==21){
-        message.textContent=("YOU DID IT BOY")
-        hasBlackJack=true
-    }
-    else{
-        message.textContent=("You never know if theres diamonds waitinging right behind this layer of rocks\n keep going!!")
-        isAlive=false
-    }
-console.log(message)
+    newC=Math.floor(Math.random()*13)+1
+    allCards.push(newC)
+    sum=sum+newC
+    render()
 }
---
+++ JS/castleObject.js
@@ -0,0 +1,9 @@
+// objects are interesting in js
+// from mu understanding they work more like dictionaries or hashmaps then actual objects
+// they still got tyhe address sstuff that goes on with java tho
+castle={
+    available:true,
+    location:"zimbabwe",
+    size:123,
+    employees:["billy","john","donaldson"]
+}
