5a888f4751823b343081ae69c715c2c321141001
1IssaMohamed <issamoh@my.yorku.ca>
2024-08-12 18:52:13 -0400

    learned how to use parameters, manipulate localStoarge to my desires and impemented it in the list along with creating a workign deleteion button

--- JS/leadTracker/extension.js
+++ JS/leadTracker/extension.js
@@ -1,24 +1,82 @@
 // function save(){
 //     console.log("buttone clicked!")
+
+// const { render } = require("express/lib/response")
+
 // }
 const inputBtn=document.getElementById("input-btn")
+const delEl=document.getElementById("delete-btn")
+
+// let myLeads=JSON.parse(localStorage.getItem("leads"))
 let myLeads=[]
+// localStorage.setItem("leads",JSON.stringify(myLeads))    
 const inputEl=document.getElementById("input-el")
 const ulEl=document.getElementById("unordered-list")
-inputBtn.addEventListener("click",function(){    
+//refresh all the links right when the page opens (if there exist any links that is)
+ const localStorageData=JSON.parse(localStorage.getItem("leads"))
+if (localStorageData){
+    myLeads=localStorageData
+    render(myLeads)
+}
+delEl.addEventListener("dblclick",function(){
+    localStorage.clear();
+    myLeads=[]
+    render(myLeads)
+})
+inputBtn.addEventListener("click", function() {
+    // The reason I dont have to get all the items form storage is because I already did that before I entered this fucntion
     myLeads.push(inputEl.value)
-        // <p><a href='abc.com' target="_blank">test</a></p>
-    // ulEl.innerHTML+= "<li> <a href='"+inputEl.value+"' target='_blank'>"+ inputEl.value+"</li>"
-    ulEl.innerHTML+= 
-    `<li>
-        <a href="${inputEl.value}" target="_blank"> 
-            ${inputEl.value}
-        </a>
-     </li>`
-
-    inputEl.value=""
+    inputEl.value = ""
+    // Save the myLeads array to localStorage 
+    // PS: remember JSON.stringify()
+    localStorage.setItem("leads",JSON.stringify(myLeads))
+    render(myLeads)
 
 })
-// for (let i=0; i<myLeads.length;i++){
-//     ulEl.textContent+=inputEl.value
-// }
+inputEl.addEventListener("keydown", function(event){
+    if(event.key=="Enter"){
+        inputBtn.click();
+    }
+})
+
+function render(leads){
+    let listItems = ""
+    for(let i=0; i< leads.length;i++){
+        listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+        </li>
+    `
+    }
+    // printint literally everythign from the localStorage isnt something you ALWAYS want to do, 
+    // sometimes the vibes may just be to print everythging out from a single vairable...
+    // for (let i = 0; i < localStorage.length; i++) {
+    //     let key = localStorage.key(i);
+    //     if (key==="leads"){
+    //     let value = JSON.parse(localStorage.getItem(key));
+    //         for (let j=0; j<value.length;j++){
+    //             listItems += `
+    //             <li>
+    //                 <a target='_blank' href='${value[j]}'>
+    //                     ${value[j]}
+    //                 </a>
+    //             </li>
+    //         `
+    //         }
+    //     }
+    //     else{
+    //         let value = JSON.stringify(localStorage.getItem(key));
+    //         listItems += `
+    //             <li>
+    //                 <a target='_blank' href='${value}'>
+    //                     ${value}
+    //                 </a>
+    //             </li>
+    //         `
+    //     }
+    ulEl.innerHTML = listItems  
+
+    }
+
