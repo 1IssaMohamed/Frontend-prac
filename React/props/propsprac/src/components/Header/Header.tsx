ace7abeb3260193d89fa4a26245d556a3364f17a
1IssaMohamed <issamoh@my.yorku.ca>
2025-06-23 08:46:28 -0400

    props stuff

---
+++ React/props/propsprac/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import './Header.css'
+import globeimg from '../../assets/Globe.png'
+// Cont with cleaning up the css and gettign the flexing to work
+
+function Header() {
+    return (
+        <>
+            <header>
+                <img src={globeimg} alt="Globe" className="globe-img" />
+                <h1>My travel journal.</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header
