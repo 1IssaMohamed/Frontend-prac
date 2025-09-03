00c13561d68de3b3f68adf8bfce2f42a69f56f6b
1IssaMohamed <issamoh@my.yorku.ca>
2025-09-03 17:06:35 -0400

    cont conditionals

--- React/State/chefclaude/src/components/Main.js
+++ React/State/chefclaude/src/components/Main.js
@@ -1,34 +1,44 @@
 import React from 'react'
-import {useState} from 'react';
+import { useState } from 'react';
 
 const Main = () => {
   // currenlty theres an array of itmes that maps the thigns in my list
   // what I want to do is make that array a use state one so that when I end up chanign the array the whole page refreshes!
-  const [ingredients,setIngredients]= useState([])
+  const [ingredients, setIngredients] = useState([])
 
-  const ingrerdientList=ingredients.map((item) => (
-          <li >{item}</li>
-        ))
+  const ingrerdientList = ingredients.map((item) => (
+    <li >{item}</li>
+  ))
 
   function printer(formData) {
-    const newIng=formData.get("ingredient");
+    const newIng = formData.get("ingredient");
     setIngredients(prevIngredients => [...prevIngredients, newIng]);
   }
 
-return (
+  return (
     <main>
-      <form class ="add-ingredient-form" action={printer}>
-        <input  
-          type="text" 
-          placeholder="Type your message..."  
+      <form class="add-ingredient-form" action={printer}>
+        <input
+          type="text"
+          placeholder="Type your message..."
           aria-label="Add ingredient"
           name="ingredient"
-          />
+        />
         <button type="submit" >Add ingredient</button>
       </form>
-      <ul>
-        {ingrerdientList}
-      </ul>
+      {ingredients.length > 0 ?
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">{ingrerdientList}</ul>
+          {ingredients.length > 3 ? <div className="get-recipe-container">
+            <div>
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients.</p>
+            </div>
+            <button>Get a recipe</button>
+          </div> : <></>}
+        </section>
+        : <></>}
     </main>
   )
 }
